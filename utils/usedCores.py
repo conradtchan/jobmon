36aee20138999f907a0b03491cc2be2d196b372e
Robin Humble <plaguedbypenguins@gmail.com>
2015-01-10 01:53:58 +1100

    update usedCores for the ganglia python modules and also for gpus

--- utils/usedCores.py
+++ utils/usedCores.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
 import os, sys, time, socket
+import gmetric
 
-gmetric = '/usr/bin/gmetric'
 gatherTime = 60
-host='vu-pbs'
+
+# gmond to send data to
+gmondHost = '192.168.55.13'  # g2 IPoIB
+gmondPort = 8650
+gmondProtocol = 'udp'   # udp or multicast
+
+# spoof host
+host = 'pbs'
 
 first=1
 
@@ -33,6 +40,7 @@ spoofStr = ip + ':' + host
 while 1:
    if first:
       first = 0
+      gm = gmetric.Gmetric( gmondHost, gmondPort, gmondProtocol )
    else:
       time.sleep(gatherTime)
 
@@ -46,29 +54,33 @@ while 1:
          #print l0
          if len(l0) != 6 or l0[:6] != '<usage' or len(l) < 3:
             continue
+         # expect
+         #  ["pbs_running_cores",%d,"pbs_avail_cores",%d,"pbs_running_gpus",%d,"pbs_avail_gpus",%d]
+
          #print 'l0',l0
          l = l[1].split('[')[1]
          l = l.split(']')[0]
          l = l.split(',')
          #print 'l',l
-         pbsRunning = int(l[1])
-         pbsAvail = int(l[3])
+         pbsRunningCores= int(l[1])
+         pbsAvailCores = int(l[3])
+         pbsRunningGpus = int(l[5])
+         pbsAvailGpus = int(l[7])
       f.close()
    except:
       s = 'logger "' + sys.argv[0] + ': harvest: Unexpected error'
       continue
 
-   #print 'pbs running, avail', pbsRunning, pbsAvail
+   #print 'pbs running, avail cores', pbsRunningCores, pbsAvailCores, 'gpus', pbsRunningGpus, pbsAvailGpus
    #sys.exit(1)
    #continue
 
    try:
-      # non-spoofing
-      #os.system( gmetric + ' -t uint32 -n "pbs_alloc_cores" -v %d' % pbsRunning )
-      #os.system( gmetric + ' -t uint32 -n "pbs_avail_cores" -v %d' % pbsAvail )
       # spoofing
-      os.system( gmetric + ' -S ' + spoofStr + ' -t uint32 -n "pbs_alloc_cores" -v %d' % pbsRunning )
-      os.system( gmetric + ' -S ' + spoofStr + ' -t uint32 -n "pbs_avail_cores" -v %d' % pbsAvail )
+      gm.send( 'pbs_alloc_cores', '%d' % pbsRunningCores, 'uint32', 'CPUs', 'both', 60, 0, '', spoofStr )
+      gm.send( 'pbs_avail_cores', '%d' % pbsAvailCores,   'uint32', 'CPUs', 'both', 60, 0, '', spoofStr )
+      gm.send( 'pbs_alloc_gpus',  '%d' % pbsRunningGpus,  'uint32', 'GPUs', 'both', 60, 0, '', spoofStr )
+      gm.send( 'pbs_avail_gpus',  '%d' % pbsAvailGpus,    'uint32', 'GPUs', 'both', 60, 0, '', spoofStr )
    except:
       s = 'logger "' + sys.argv[0] + ': feeding to gmetric: Unexpected error: ' + str(sys.exc_info()[0]) + '"'
       os.system(s)
