f745ca3e3096aaf9e736e228ad032ef353a764fb
Conrad Chan <chan_con@hotmail.com>
2020-08-27 18:18:19 +1000

    black formatting

--- backend/file_utils.py
+++ backend/file_utils.py
@@ -3,15 +3,17 @@ import gzip
 import json
 import stat
 
+
 def mode644():
-    return (stat.S_IRUSR|stat.S_IWUSR|stat.S_IRGRP|stat.S_IROTH)
+    return stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH
+
 
 def write_data(data, filename):
     # Write to a temporary file, because it takes time
-    tmp_filename = filename + '.new'
+    tmp_filename = filename + ".new"
     json_str = json.dumps(data)
-    json_bytes = json_str.encode('utf-8')
-    with gzip.open(tmp_filename, 'wb') as f:
+    json_bytes = json_str.encode("utf-8")
+    with gzip.open(tmp_filename, "wb") as f:
         f.write(json_bytes)
     chmod(tmp_filename, mode644())
 
@@ -20,9 +22,9 @@ def write_data(data, filename):
         rename(tmp_filename, filename)
     except (OSError, error):
         err_num, err_str = error
-        print('write_data: renamed failed. OsError', error)
-        if err_num == 13: # Permission denied
-            print('Permission denied (probably running as a user)')
+        print("write_data: renamed failed. OsError", error)
+        if err_num == 13:  # Permission denied
+            print("Permission denied (probably running as a user)")
 
         # Unlink temp file
         unlink(tmp_filename)
