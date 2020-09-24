ff7d3a5471447ea0ee6265a94ef8559c8b0b4cfb
Conrad Chan <chan_con@hotmail.com>
2020-09-24 10:16:04 +1000

    why-did-you-render checking

---
+++ frontend/src/wdyr.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+if (process.env.NODE_ENV === 'development') {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render'); // eslint-disable-line global-require
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+  });
+}
