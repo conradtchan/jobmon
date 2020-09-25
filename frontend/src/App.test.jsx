88bb2df5a5b99880dd9453f9a8ffc60452270db3
Conrad Chan <chan_con@hotmail.com>
2020-09-25 12:10:51 +1000

    test data format

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const testData = require('../test/bobData_test.json')
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
@@ -11,4 +12,12 @@ it('renders without crashing', () => {
 // sanity check
 it('one is one', () => {
   expect(1).toEqual(1);
+});
+
+it('test data format', () => {
+  const keys = Object.keys(testData)
+  expect(keys).toEqual(expect.arrayContaining(
+    ['api', 'timestamp', 'nodes', 'jobs']
+  ))
+
 });
