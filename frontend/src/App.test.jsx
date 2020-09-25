50360cb717ac3a49f7359462842829fb835fd23d
Conrad Chan <chan_con@hotmail.com>
2020-09-25 13:09:05 +1000

    mock requests

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import fetchMock from 'fetch-mock';
 
 const testData = require('../test/bobData_test.json')
+const testHistory = require('../test/history.json')
+const testBackfill = require('../test/history.json')
+
+fetchMock.get('end:bobdata.py', testData);
+fetchMock.get('end:bobhistory.py', testHistory);
+fetchMock.get('end:bobbackfill.py', testBackfill);
+
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
