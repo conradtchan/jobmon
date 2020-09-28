d2feab6013c2289dd3274f8805844c5345befacc
Conrad Chan <chan_con@hotmail.com>
2020-09-29 09:48:26 +1000

    test data for warnings

--- frontend/src/warnings.test.js
+++ frontend/src/warnings.test.js
@@ -1,7 +1,21 @@
-import { instantWarnings } from './warnings';
+import generateWarnings, { instantWarnings } from './warnings';
 
 const testData = require('../test/bobData_test.json');
+const instantRef = require('../test/instantWarnings_reference.json');
+
+const testHistoryData = require('../test/historyData_test.json');
+const scoreRef = require('../test/warningSums_reference.json');
 
 it('instantaneous warnings', () => {
-  instantWarnings(testData);
+  const w = instantWarnings(testData);
+
+  // Check that objects are the same
+  expect(w).toEqual(instantRef);
+});
+
+it('warning scores', () => {
+  const snapshotTime = new Date(testHistoryData[testHistoryData.length - 1].timestamp);
+  const w = generateWarnings(snapshotTime, testHistoryData);
+
+  expect(w).toEqual(scoreRef);
 });
