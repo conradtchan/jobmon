3632b6f34a441ce2d1294861aa61a8b9f3a8cbd7
Conrad Chan <chan_con@hotmail.com>
2020-09-29 11:55:25 +1000

    wdyr config

--- frontend/src/wdyr.js
+++ frontend/src/wdyr.js
@@ -2,7 +2,5 @@ import React from 'react';
 
 if (process.env.NODE_ENV === 'development') {
   const whyDidYouRender = require('@welldone-software/why-did-you-render'); // eslint-disable-line global-require
-  whyDidYouRender(React, {
-    trackAllPureComponents: true,
-  });
+  whyDidYouRender(React, {});
 }
