c4cc34873b14b19c2ea311d2909ce3e4d7dfe869
Conrad Chan <chan_con@hotmail.com>
2020-10-01 13:34:51 +1000

    remove test arg from cron script

--- backend/jobmon.sh
+++ backend/jobmon.sh
@@ -8,6 +8,6 @@ cd /home/cchan/backend
 export PYTHONPATH=$PYTHONPATH:/apps/pyslurm/19.05.0/lib64/python3.4/site-packages
 export RRDCACHED_ADDRESS=unix:/mnt/rrd/var_lib/ganglia/rrds/rrdcached.sock
 
-/usr/bin/python3 jobmon.py test
+/usr/bin/python3 jobmon.py
 
 ) 9>/home/cchan/backend/.lockfile
