3c52cb1b9e989539629968e58d7de987839e96d7
Conrad Chan <conradchan@hotmail.com.au>
2021-05-04 14:22:27 +1000

    options for JOBMON_DIR and LOCKFILE in jobmon.sh

--- backend/jobmon.sh
+++ backend/jobmon.sh
@@ -3,11 +3,17 @@
 (
 flock -n 9 || exit 1
 
-cd /home/cchan/backend
+# Location of backend script
+JOBMON_DIR=/home/cchan/backend
 
+# Lockfile to prevent multiple instances running
+LOCKFILE=/tmp/.jobmon.lockfile
+
+# Dependencies
 export PYTHONPATH=$PYTHONPATH:/apps/pyslurm/20.02.6/lib64/python3.6/site-packages
 export RRDCACHED_ADDRESS=unix:/mnt/rrd/var_lib/ganglia/rrds/rrdcached.sock
 
+cd $JOBMON_DIR
 /usr/bin/python3 jobmon.py
 
-) 9>/home/cchan/backend/.lockfile
+) 9>$LOCKFILE
