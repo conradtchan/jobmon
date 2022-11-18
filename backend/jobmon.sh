1adc4d321352ca6d7e750b02d7257041d73277bc
Conrad Chan <conradchan@hotmail.com.au>
2022-11-18 18:40:18 +1100

    add influxdb python path to jobmon.sh

--- backend/jobmon.sh
+++ backend/jobmon.sh
@@ -7,7 +7,8 @@ JOBMON_DIR=/home/cchan/backend
 LOCKFILE=/tmp/.jobmon.lockfile
 
 # Dependencies
-export PYTHONPATH=$PYTHONPATH:/apps/pyslurm/20.02.6/lib64/python3.6/site-packages
+export PYTHONPATH=/apps/pyslurm/20.02.6/lib64/python3.6/site-packages:/apps/influxdb-client/1.34.0-py3/lib64/python3.6/site-packages:$PYTHONPATH
+
 export RRDCACHED_ADDRESS=unix:/mnt/rrd/var_lib/ganglia/rrds/rrdcached.sock
 
 (
